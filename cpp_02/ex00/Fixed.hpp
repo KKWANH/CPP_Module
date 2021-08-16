d81296ed894192680236de5dd662cf0aa9af480a
KKWANH <kwanho0096@gmail.com>
2021-08-16 13:14:13 +0900

    🗿 CPP_02 in progress

---
+++ cpp_02/ex00/Fixed.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kimkwanho <kimkwanho@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/15 22:54:11 by kimkwanho         #+#    #+#             */
+/*   Updated: 2021/08/15 22:55:13 by kimkwanho        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef			FIXED_HPP
+# define			FIXED_HPP
+
+# include			<iostream>
+
+class				Fixed
+{
+	private:
+		int			_val;
+
+	public:
+		Fixed(void);
+		~Fixed(void);
+};
+
+
+# endif
